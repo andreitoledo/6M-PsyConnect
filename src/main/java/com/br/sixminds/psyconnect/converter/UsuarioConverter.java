0db07648f993c84df29982edd1bd194545d6242d
Andrei Toledo <andreiltoledo@hotmail.com>
2019-01-23 02:09:18 -0200

    ajuste no converter usuario

--- src/main/java/com/br/sixminds/psyconnect/converter/UsuarioConverter.java
+++ src/main/java/com/br/sixminds/psyconnect/converter/UsuarioConverter.java
@@ -9,7 +9,11 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
+
+import com.br.sixminds.psyconnect.dao.UsuarioDAO;
 import com.br.sixminds.psyconnect.model.Usuario;
 import com.br.sixminds.psyconnect.repository.Usuarios;
 import com.br.sixminds.psyconnect.util.cdi.CDIServiceLocator;
@@ -18,23 +22,28 @@ import com.br.sixminds.psyconnect.util.cdi.CDIServiceLocator;
  * @author andrei
  *
  */
-@FacesConverter(forClass=Usuario.class)
-public class UsuarioConverter implements Converter{
+@FacesConverter(forClass = Usuario.class)
+public class UsuarioConverter implements Converter {
 	
-	//@Inject
+	/*implementação para a verificação do usuário no login*/
 	private Usuarios usuarios;
 	
-	public UsuarioConverter(){
+	public UsuarioConverter() {
 		this.usuarios = (Usuarios) CDIServiceLocator.getBean(Usuarios.class);
 	}
 	
-	
+
+	@Inject
+	private UsuarioDAO usuarioDAO;
+
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		
 		Usuario retorno = null;
 
-		if (value != null) {
-			retorno = this.usuarios.porCodigo(new Long(value));
+		if (StringUtils.isNotEmpty(value)) {
+			retorno = this.usuarioDAO.buscarPeloCodigo(new Long(value));
+
 		}
 
 		return retorno;
@@ -42,12 +51,16 @@ public class UsuarioConverter implements Converter{
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component, Object value) {
+
 		if (value != null) {
-			return ((Usuario) value).getCodigo().toString();
+
+			Long codigo = ((Usuario) value).getCodigo();
+			String retorno = (codigo == null ? null : codigo.toString());
+
+			return retorno;
 		}
+
 		return "";
 	}
-	
-	
 
 }
